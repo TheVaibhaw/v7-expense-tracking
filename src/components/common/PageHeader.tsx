c6d8f173277de1dc74888369a0ea91c76635d479
Vaibhaw Kumar <imvaibhaw@gmail.com>
2026-01-02 23:47:07 +0530

    feat: Initialize Next.js root layout with metadata, global styles, `ExpenseProvider`, and `Toaster`.

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -1,11 +1,14 @@
 import Logo from '@/components/common/Logo';
+import AddExpenseButton from '@/components/expense/AddExpenseButton';
 
 const PageHeader = () => {
   return (
     <div className="flex items-center justify-between space-y-2 mb-4">
       <Logo />
+      <AddExpenseButton />
     </div>
   );
 };
 
 export default PageHeader;
+
